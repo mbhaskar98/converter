44a2f692adb9aabaeb3df37f622628bcb1d96b00
Bhaskar Mahajan <bmahajan@zscaler.com>
2024-06-03 14:49:33 +0530

    init

---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by Bhaskar Mahajan on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
